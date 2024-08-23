b0dbad85f91e1d20a32d195417bdd670c322597b
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-08-23 22:59:03 +0530

    arrow and hover

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { GoArrowUpRight } from "react-icons/go";
 
 function Landing() {
   return (
-    <div className='w-full h-screen bg-zinc-800 '>
+    <div className='w-full h-screen bg-zinc-800'>
       <div className='text-white px-10 py-32 text-9xl tracking-tighter uppercase bold leading-[0.9]'>
         <h1>we create</h1>
         <div className='flex items-center'>
-          <img className='image rounded-md w-40 h-24' src="https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg" alt="d"/>
-        <h1>eye-opening</h1>
+          <img className='image rounded-md w-40 h-24' src="https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg" alt="d" />
+          <h1>eye-opening</h1>
         </div>
         <h1>presentations</h1>
       </div>
-      <div className='border-t border-zinc-700 flex justify-between items-center '>
-        {['for public and private users','from first pitch to IPO','start the projects'].map((item,index)=>{
-          return <h3 key={index} className='text-white text-lg ml-11 mr-11'>{item}</h3>
-        })
-
-  }
+      <div className='border-t border-zinc-700 p-4 flex justify-between items-center'>
+        {['for public and private users', 'from first pitch to IPO', 'start the projects'].map((item, index) => (
+          <div key={index} className='flex items-center'>
+            <h3 className={
+              index === 2
+                ? 'border-white p-2 border-[1px] rounded-full text-white uppercase text-lg ml-11 mr-2  hover:bg-white hover:text-black'
+                : 'text-white text-lg ml-11 mr-11 '
+            }>
+              {item}
+            </h3>
+            {index === 2 && <GoArrowUpRight className='text-white m-1 h-10 w-10 border-white border-[1px] rounded-full hover:bg-white hover:text-black' />}
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
