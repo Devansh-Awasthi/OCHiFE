5ed6e4d32246113f07d899a310bf1bb7f2f4cc66
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-08-29 19:53:36 +0530

    marquee complete

--- src/Components/Marq.jsx
+++ src/Components/Marq.jsx
@@ -4,13 +4,12 @@ import React from 'react'
 function Marq() {
   
   return (
-    <div className='h-[60vh] w-full bg-[#076e93] overflow-hidden  rounded-xl translate-y-[-5%] flex text-white items-center text-[15.6vw] font-arial-regular uppercase -tracking-[1.4rem] '>
-        <motion.div initial={{x:0}} animate={{x:'100%'}} transition={{ease:'linear',repeat:Infinity,duration:5}} className=' text-white flex overflow-hidden font-medium animate-marquee whitespace-nowrap '>
-       {['we are ochie',' we are ochie'].map((item,index)=>{
-       return <h1  key={index}>{item} </h1>
-       
+    <div className='h-[60vh] w-full bg-[#076e93] overflow-hidden p-0 m-0 rounded-xl translate-y-[-5%] flex text-white items-center text-[17vw] font-arial-regular uppercase -tracking-[1.5rem] '>
+        <motion.div initial={{x:'0%'}} animate={{x:'-75%'}} transition={{ease:'linear',repeat:Infinity,duration:12}} className='text text-white flex p-0 font-medium whitespace-nowrap border-t-2 border-white  border-b-2 '>
+       {['we are ochi',' we are ochi','we are ochi',' we are ochi'].map((item,index)=>{
+       return <h1 className='overflow-hidden -mt-12 -mb-16 p-0 mr-10' key={index}>{item} </h1>
        })}
-    
+      
     
       </motion.div>
     </div>
