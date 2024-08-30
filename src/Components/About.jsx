a9972fcac049a3051ff0b4fd0e09fc5c60da5e4d
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-08-30 12:10:01 +0530

    About continues...

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,30 +5,35 @@ function About() {
     <div className='h-screen w-full translate-y-[-4%] rounded-lg bg-[#CDEA68]'>
         <p className='text-[4.3rem] pt-20 pl-16 pr-36 pb-28 leading-[4rem]'>Ochi is a strategic presentation agency for forward-thinking businesses that need to <u>raise funds</u>,<u> sell products</u>, <u>explain complex ideas</u>, and <u>hire great peo-ple.</u></p>
         <div className='border-[0.5px] border-zinc-600'></div>
-        <div className='pl-16 w-[100%] h-36 flex  justify-between '>
-            <div className='flex w-[50%] h-[100%]'>What you can expect:  </div>
+        <div className='pl-16 m-6 w-[100%] h-36 flex  justify-between text-lg'>
+            <div className='flex w-[60%] h-[100%]'>What you can expect:  </div>
             <div className='flex justify-between pl-32 pr-32'>
-                <div className='flex justify-between flex-col w-[25%]'>
-                        <div className=''>
+                <div className='flex justify-between flex-col w-[50%]'>
+                        <div className='pr-20 '>
 We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it's live or digital, delivered for one or a hundred people.
                         </div>
-                     <div className=' mt-14'>
+                     <div className='pr-20 mt-14'>
                      We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</div>   
                      </div>
-               <div className=''> 
+               <div className=' mr-14'> 
                 <br></br>
                 <br></br>
                 <br></br>
 
 S:
-
-Instagram
-Behance
-Facebook
-Linkedin</div>      
+<br></br><br></br>
+<h6 className='underline hover:underline-offset-4'>Linkedin</h6></div>      
+<h6 className='underline hover:underline-offset-4'>Instagram</h6>
+<h6 className='underline hover:underline-offset-4'>Behance</h6>
+<h6 className='underline hover:underline-offset-4'>Facebook</h6>
                  </div>
       
 
+        </div>
+        <div>
+
+
+            
         </div>
     </div>
   )
