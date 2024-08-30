84e4ad0a3472e2cc5149a0d89a4669280620b96e
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-08-30 11:34:09 +0530

    hALF aBOUT

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from './Components/NavBar'
 import Landing from './Components/Landing'
 import Marq from './Components/Marq'
+import About from './Components/About'
 function App() {
   return (
     <div className='h-screen w-full'>
       <NavBar></NavBar>
       <Landing></Landing>
       <Marq></Marq>
+      <About></About>
     </div>
   )
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='h-screen w-full translate-y-[-4%] rounded-lg bg-[#CDEA68]'>
+        <p className='text-[4.3rem] pt-20 pl-16 pr-36 pb-28 leading-[4rem]'>Ochi is a strategic presentation agency for forward-thinking businesses that need to <u>raise funds</u>,<u> sell products</u>, <u>explain complex ideas</u>, and <u>hire great peo-ple.</u></p>
+        <div className='border-[0.5px] border-zinc-600'></div>
+        <div className='pl-16 w-[100%] h-36 flex  justify-between '>
+            <div className='flex w-[50%] h-[100%]'>What you can expect:  </div>
+            <div className='flex justify-between pl-32 pr-32'>
+                <div className='flex justify-between flex-col w-[25%]'>
+                        <div className=''>
+We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it's live or digital, delivered for one or a hundred people.
+                        </div>
+                     <div className=' mt-14'>
+                     We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</div>   
+                     </div>
+               <div className=''> 
+                <br></br>
+                <br></br>
+                <br></br>
+
+S:
+
+Instagram
+Behance
+Facebook
+Linkedin</div>      
+                 </div>
+      
+
+        </div>
+    </div>
+  )
+}
+
+export default About
