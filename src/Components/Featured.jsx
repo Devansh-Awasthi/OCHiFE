4483be549e5ff8fc6f43a6e83346e76ad4a2142d
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-01 09:37:35 +0530

    button left

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -5,8 +5,8 @@ function Featured() {
     <div className="h-min-screen w-full bg-[#27272A]">
       <h1 className="text-white text-[4vw] p-6">Featured projects</h1>
       <div className="border-zinc-600 border-[1px] mb-8"></div>
-      <div className="flex justify-between h-[80vh] p-8 w-[100vw]">
-        <div className="left h-full w-[46vw]">
+      <div className="flex justify-between h-[90vh] p-8 w-[100vw]">
+        <div className="left max-h-fit w-[46vw]">
           <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
             <h1 className="text-lg uppercase">Cardboard Spaceship</h1>
@@ -18,10 +18,21 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")',
             }}
           ></div>
-          
+          <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 hover:text-black duration-600 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
         <div className="right h-full w-[46vw]">
-        <div className="head text-zinc-300 flex items-center mb-7">
+          <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
             <h1 className="text-lg uppercase">AH2 & Matt Horn</h1>
           </div>
@@ -32,9 +43,21 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png")',
             }}
           ></div>
+          <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 duration-900 hover:text-black duration-900 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
       </div>
-      <div className="flex justify-between h-[80vh] p-8 w-[100vw]">
+      <div className="flex justify-between h-[90vh] p-8 w-[100vw]">
         <div className="left h-full w-[46vw]">
           <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
@@ -47,9 +70,21 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png")',
             }}
           ></div>
+             <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 duration-900 hover:text-black duration-900 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
         <div className="right h-full w-[46vw]">
-        <div className="head text-zinc-300 flex items-center mb-7">
+          <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
             <h1 className="text-lg uppercase">AH2 & Matt Horn</h1>
           </div>
@@ -60,9 +95,21 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg")',
             }}
           ></div>
+             <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 duration-900 hover:text-black duration-900 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
       </div>
-      <div className="flex justify-between h-[80vh] p-8 w-[100vw]">
+      <div className="flex justify-between h-[90vh] p-8 w-[100vw]">
         <div className="left h-full w-[46vw]">
           <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
@@ -75,9 +122,21 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2023/08/Frame-3875-663x551.jpg")',
             }}
           ></div>
+             <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 duration-900 hover:text-black duration-900 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
         <div className="right h-full w-[46vw]">
-        <div className="head text-zinc-300 flex items-center mb-7">
+          <div className="head text-zinc-300 flex items-center mb-7">
             <div className="h-3 w-3 rounded-full bg-zinc-300 mr-5"></div>
             <h1 className="text-lg uppercase">AH2 & Matt Horn</h1>
           </div>
@@ -88,6 +147,18 @@ function Featured() {
                 'url("https://ochi.design/wp-content/uploads/2022/12/PB-Front-4-663x551.png")',
             }}
           ></div>
+             <div className="m-4 flex gap-7 text-zinc-400">
+            {["branded template", "sales deck", "social media templates"].map(
+              (item, index) => (
+                <h1
+                  key={index}
+                  className="pl-3 pr-3 pt-3 pb-3 uppercase rounded-full border-[1px] border-zinc-400 hover:bg-zinc-400 duration-900 hover:text-black duration-900 ease-in-out"
+                >
+                  {item}
+                </h1>
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
